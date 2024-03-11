dd5b68ac3323e6a9ff33bece42f7519fad8f4388
Alex <fnelclsrn123@gmail.com>
2024-03-12 08:51:20 +0900

    feat: 스레드 풀 생성 cpu의 스레드 개수에 맞춰서 스레드 풀 생성하고 활용하도록 함

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,33 +1,48 @@
 package webserver;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+        int port = determinePort(args);
+        generateSocket(port);
+    }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
+    private static int determinePort(String[] args) {
+        if (args != null && args.length > 0) {
+            return Integer.parseInt(args[0]);
+            //port
+        }
+        //웹서버는 기본적으로 8080번 포트를 사용한다.
+        return DEFAULT_PORT;
+    }
+
+    private static void generateSocket(int port) throws IOException {
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                startThread(connection);
             }
         }
+    }
+
+    private static void startThread(Socket connection) {
+        //Runtime.getRuntime().availableProcessors()을 통해서 cpu 스레드가 4개로 나와서 이에 맞춰 스레드 개수 설정함
+        //아직까지 비동기나 동기화와 관련된 요구사항이 없어서 단순한 스레드풀을 생성함
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        executorService.submit(new RequestHandler(connection));
     }
 }
