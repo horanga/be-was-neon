c790bf509cdb4525e82a1bbaaf197525cde4bf1a
Alex <fnelclsrn123@gmail.com>
2024-03-12 08:53:30 +0900

    feat: http request header 출력 기능 추가

--- src/main/java/util/HeaderPrinter.java
+++ src/main/java/util/HeaderPrinter.java
@@ -1,4 +1,16 @@
-package utility;
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class HeaderPrinter {
+
+    public static void printRequestHeader(String httpRequestHeader, BufferedReader br) throws IOException {
+        System.out.println(httpRequestHeader);
+
+        while (!httpRequestHeader.isEmpty()) {
+            httpRequestHeader = br.readLine();
+            System.out.println(httpRequestHeader);
+        }
+    }
 }
