e2b534390c625561a2bc74c34f5b9663d6e8c026
Alex <fnelclsrn123@gmail.com>
2024-03-12 08:55:17 +0900

    feat: request 요청에 따른 로직을 처리하는지 테스트함 실제 8080포트에 연결하면 시간이 소요되기에 mock 테스트를 함

--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -9,10 +9,10 @@ import java.net.Socket;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class WebServerTest {
+class RequestHandlerTest {
 
 
-    @DisplayName("소켓이 정상 연결된다")
+    @DisplayName("HTTP 요청 처리가 정상 작동하는지 검증한다")
     @Test
     void test3() throws IOException {
         Socket socketMock = mock(Socket.class);
