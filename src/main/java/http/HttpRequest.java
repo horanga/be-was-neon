5b322fe023941c912fcb5ad7d71e46d58daa119e
Alex <fnelclsrn123@gmail.com>
2024-03-12 17:20:37 +0900

    refactor: HttpRequest 객체 생성 reqeust를 처리하는 객체 생성

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,2 +1,24 @@
-package Http;public class HttpRequest {
+package http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class HttpRequest {
+
+    public String[] getRequest(BufferedReader br) throws IOException {
+
+        String requestHeader;
+        String urlRequest;
+        requestHeader = br.readLine();
+        urlRequest = parseRequest(requestHeader);
+
+        return new String[]{requestHeader, urlRequest};
+    }
+
+
+    public String parseRequest(String input) {
+        String[] requestSplits = input.replaceAll("\\/", "").split(" ");
+        return requestSplits[1].trim();
+        //requestSplits[1]=요청 URL
+    }
 }
