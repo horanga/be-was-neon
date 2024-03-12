8500309de12fbae6ef2c0dbf92b0669c9221cdae
Alex <fnelclsrn123@gmail.com>
2024-03-12 17:18:42 +0900

    refactor: 버퍼를 인자로 받는 방식으르 변화

--- src/main/java/util/HeaderPrinter.java
+++ src/main/java/util/HeaderPrinter.java
@@ -6,15 +6,21 @@ import webserver.RequestHandler;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HeaderPrinter {
-    public static void printRequestHeader(String httpRequestHeader, BufferedReader br) throws IOException {
+    public static void printRequestHeader(BufferedReader br, String httpRequestHeader) {
+
         Logger log = LoggerFactory.getLogger(RequestHandler.class);
         log.debug(httpRequestHeader);
 
         while (!httpRequestHeader.isEmpty()) {
-            httpRequestHeader = br.readLine();
-           log.debug(httpRequestHeader);
+            try {
+                httpRequestHeader = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            log.debug(httpRequestHeader);
         }
     }
 }
