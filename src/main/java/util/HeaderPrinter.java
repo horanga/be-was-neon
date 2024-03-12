e35de3b0880b8b7955fa7909ea6408bebfbdf532
Alex <fnelclsrn123@gmail.com>
2024-03-12 14:05:16 +0900

    refactor: HeaderPrinter log 사용하는 방식으로 변경 println방식은 출력되고 사라지기 때문에 기록이 남는 log로 변경

--- src/main/java/util/HeaderPrinter.java
+++ src/main/java/util/HeaderPrinter.java
@@ -1,16 +1,20 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class HeaderPrinter {
-
     public static void printRequestHeader(String httpRequestHeader, BufferedReader br) throws IOException {
-        System.out.println(httpRequestHeader);
+        Logger log = LoggerFactory.getLogger(RequestHandler.class);
+        log.debug(httpRequestHeader);
 
         while (!httpRequestHeader.isEmpty()) {
             httpRequestHeader = br.readLine();
-            System.out.println(httpRequestHeader);
+           log.debug(httpRequestHeader);
         }
     }
 }
