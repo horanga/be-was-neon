7bb51d8e207b1796e87be1405efa7a0b82bf46eb
Alex <fnelclsrn123@gmail.com>
2024-03-13 22:18:33 +0900

    refactor: 주소 수정

--- src/main/java/http/request/Membership.java
+++ src/main/java/http/request/Membership.java
@@ -3,9 +3,11 @@ package http.request;
 import java.io.IOException;
 
 public class Membership implements HttpRequest {
+
+
     @Override
     public String getUri() throws IOException {
-        return "membership";
+        return "registration/index.html";
 
         //회원가입 폼을 제출하는 uri임을 식별하하도록 도와주는 문자열
     }
