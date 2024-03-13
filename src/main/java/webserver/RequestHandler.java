96b2d711268212c06b4f2463fa7317075f021d6d
Alex <fnelclsrn123@gmail.com>
2024-03-13 22:19:19 +0900

    refactor: request, response 인터페이스로 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,7 +1,10 @@
 package webserver;
 
-import http.HttpRequest;
-import http.HttpResponse;
+import http.factory.JoinUriFactory;
+import http.factory.PageUriFactory;
+import http.factory.ResponseFactory;
+import http.request.HttpRequest;
+import http.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HeaderPrinter;
@@ -11,33 +14,48 @@ import java.net.Socket;
 
 public class RequestHandler implements Runnable {
     public static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    HttpRequest httpRequest;
-    HttpResponse httpResponse;
+
     private Socket connection;
 
-    public RequestHandler(Socket connectionSocket, HttpRequest httpRequest, HttpResponse httpResponse) {
+    public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
-        this.httpRequest = httpRequest;
-        this.httpResponse = httpResponse;
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        debugIp();
 
         try (InputStream in = connection.getInputStream();
-             OutputStream out = connection.getOutputStream();
              InputStreamReader inputStreamReader = new InputStreamReader(in);
              BufferedReader br = new BufferedReader(inputStreamReader)) {
 
-            String[] request = httpRequest.getRequest(br);
-            HeaderPrinter.printRequestHeader(br, request[0]);
-            httpResponse.sendReponsed(request[1], out);
-
-            //request[0] = requestHeader, request[0]= urlRequest
+            String requestHeader = br.readLine();
+            HeaderPrinter.printRequestHeader(br, requestHeader);
+            HttpRequest uri = getRequestType(requestHeader);
+            respond(uri, requestHeader);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private HttpRequest getRequestType(String requestHeader) throws IOException {
+        return requestHeader.contains("?") ?
+                new JoinUriFactory().getRequest(requestHeader) :
+                new PageUriFactory().getRequest(requestHeader);
+    }
+
+    private void respond(HttpRequest requestType, String requestHeader) {
+        HttpResponse response = ResponseFactory.getResponse(requestType);
+        try (OutputStream out = connection.getOutputStream()) {
+            response.respondToRequest(requestType, out, requestHeader);
+        } catch (IOException e) {
+            logger.debug(e.getMessage());
+
+        }
+    }
+
+    private void debugIp() {
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                connection.getPort());
     }
 }
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,7 +1,5 @@
 package webserver;
 
-import http.HttpRequest;
-import http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,28 +12,28 @@ import java.util.concurrent.Executors;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
-    private static final HttpRequest httpRequest = new HttpRequest();
-    private static final HttpResponse httpResponse = new HttpResponse();
     private static final ExecutorService executorService = Executors.newFixedThreadPool(4);
+
     //Runtime.getRuntime().availableProcessors()을 통해서 cpu 스레드가 4개로 나와서 이에 맞춰 스레드 개수 설정함
     //아직까지 비동기나 동기화와 관련된 요구사항이 없어서 단순한 스레드풀을 생성함
 
     public static void main(String args[]) throws Exception {
         int port = determinePort(args);
-        generateSocket(port);
+        connectSocket(port);
     }
 
     private static int determinePort(String[] args) {
         if (args != null && args.length > 0) {
             return Integer.parseInt(args[0]);
-            //적어도 포트를 한줄 이상하면 그 숫자에 맞춰서 소켓 생성
+            //적어도 포트를 한줄 이상 입력하면 그 숫자에 맞춰서 소켓 생성
             //커맨드라인 입력창이 String으로 입력을 받아서 숫자로 변환함
         }
         return DEFAULT_PORT;
         //입력받은 값이 없으면 내 컴퓨터의 포트번호인 8080로 접속한다.
     }
 
-    private static void generateSocket(int port) throws IOException {
+    private static void connectSocket(int port) throws IOException {
+
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
@@ -48,7 +46,6 @@ public class WebServer {
     }
 
     private static void startThread(Socket connection) {
-
-        executorService.submit(new RequestHandler(connection, httpRequest, httpResponse));
+        executorService.submit(new RequestHandler(connection));
     }
 }
