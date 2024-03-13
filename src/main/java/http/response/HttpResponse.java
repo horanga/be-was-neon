9055ab89fb7780afae7b5fc36049ca9d98d90add
Alex <fnelclsrn123@gmail.com>
2024-03-13 22:17:45 +0900

    feat: JoinResponse 생성 response를 인터페이스로 활용하도록 수정

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,14 +1,25 @@
 package http.response;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import http.request.HttpRequest;
+
+import java.io.*;
 
 import static webserver.RequestHandler.logger;
 
 public interface HttpResponse {
 
-    public void respondToRequest(String urlRequest, OutputStream out) throws IOException;
+    public void respondToRequest(HttpRequest httpRequest, OutputStream out, String requstHeader) throws IOException;
+
+    default byte[] getRequestedFile(String urlRequest) throws IOException {
+        final String relativePath = "src/main/resources/static/";
+        File file = new File(relativePath, urlRequest);
+
+        byte[] data = new byte[(int) file.length()];
+        try (FileInputStream fis = new FileInputStream(file)) {
+            fis.read(data);
+        }
+        return data;
+    }
 
     default void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
--- src/main/java/http/response/JoinResponse.java
+++ src/main/java/http/response/JoinResponse.java
@@ -1,11 +1,25 @@
 package http.response;
 
+import db.Database;
+import http.request.HttpRequest;
+import model.User;
+import util.Parsor;
+
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 
 public class JoinResponse implements HttpResponse{
     @Override
-    public void respondToRequest(String urlRequest, OutputStream out) throws IOException {
+    public void respondToRequest(HttpRequest httpRequest, OutputStream out, String requstHeader) throws IOException {
+        byte[] response = getRequestedFile(httpRequest.getUri());
+        DataOutputStream dos = new DataOutputStream(out);
+        response200Header(dos, response.length);
+        responseBody(dos, response);
 
+        List<String> strings = Parsor.pasrseJoinRequest(requstHeader);
+        Database.addUser(new User(strings.get(0),strings.get(1), strings.get(2), strings.get(3) ));
+        //userId, password, name, email순으로 저장한다.
     }
 }
