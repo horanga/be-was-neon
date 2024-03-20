bb1d9c566e36f0b5318f74d587eeaa9d24c3f938
Alex <fnelclsrn123@gmail.com>
2024-03-20 22:22:27 +0900

    refactor: 변경사항 저장

---
+++ src/test/java/request/RequestTest.java
@@ -0,0 +1,35 @@
+package request;
+
+import http.request.message.MessageParser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestTest {
+
+    //테스트코드 수정할 예정
+
+//    @DisplayName("url의 뎁스가 3인 리퀘스트 요청에서 리소스 경로를 파싱한다.")
+//    @Test
+//    void test1(){
+//        MessageParser requestParser = new MessageParser();
+//
+//        String requestLine = "GET /users/boards/category HTTP/1.1";
+//        String[] parse = requestParser.parseRequestMessage(requestLine);
+//
+//        assertThat(parse).isEqualTo(new String[]{"users", "boards", "category"});
+//
+//    }
+//
+//    @DisplayName("뎁스가 0인 url은 반환할 리소스 경로를 갖지 않는다. ")
+//    @Test
+//    void test2(){
+//        Parser requestParser = new Parser();
+//
+//        String requestLine = "GET / HTTP/1.1";
+//        String[] parse = requestParser.parsePageRequest(requestLine);
+//
+//        assertThat(parse).isEqualTo(new String[]{});
+//    }
+}
