3911d938445aed7e205dbcad4b0a397aee5a5916
Alex <fnelclsrn123@gmail.com>
2024-03-20 21:46:11 +0900

    refactor: printer를 static 메서드에서 인스턴스 메서드로 변경

---
+++ src/main/java/util/MessagePrinter.java
@@ -0,0 +1,16 @@
+package util;
+
+import http.request.message.RequestMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+public class MessagePrinter {
+    public void printRequestHeader(RequestMessage message) {
+
+        Logger log = LoggerFactory.getLogger(RequestHandler.class);
+        log.debug(message.getRequestLine().getRequestLine());
+
+        message.getMessage().stream().forEach(i -> log.debug(i));
+    }
+}
