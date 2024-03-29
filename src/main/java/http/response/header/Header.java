6b7bb4712c64a6c33db4cc3d7944fb6acf2921f0
Alex <fnelclsrn123@gmail.com>
2024-03-29 14:14:21 +0900

    refactor: 헤더에 로그인 성공 기록 추가

--- src/main/java/http/response/header/Header.java
+++ src/main/java/http/response/header/Header.java
@@ -1,5 +1,8 @@
 package http.response.header;
 
+import http.ClientDatabase;
+import login.SessionManager;
+
 public class Header {
     //리퀘스트랑 리스판스랑 헤더가 같다.
 
@@ -9,7 +12,7 @@ public class Header {
     private  String contentType;
     private  int contentLength;
     private String newLocation;
-
+    SessionManager sessionManager = new SessionManager();
     public Header(){};
 
 
@@ -29,6 +32,15 @@ public class Header {
         header.newLocation = newLocation;
 
         return header;
+    }
+
+    public String getLoginSuccesMessage(String sid){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(getMessage());
+        stringBuilder.append("Set-Cookie: sid=" + sid+";"+" Path="+carriageReturn);
+        stringBuilder.append(carriageReturn);
+
+        return stringBuilder.toString();
     }
 
     public String getMessage() {
