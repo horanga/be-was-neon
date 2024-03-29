110c2071d36ff1486dfd32d9b79eb337b8171029
Alex <fnelclsrn123@gmail.com>
2024-03-29 14:11:54 +0900

    refactor: 리다이렉트를 http로 보내는 sender 클래스 생성

---
+++ src/main/java/http/response/Responsesender/NormalResponseSender.java
@@ -0,0 +1,23 @@
+package http.response.Responsesender;
+
+import http.response.Response;
+import http.response.ResponseSender;
+import http.response.header.Header;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+
+public class NormalResponseSender implements ResponseSender {
+    @Override
+    public void sendResponse(byte[] file,  Response response, OutputStream out) {
+
+        DataOutputStream dos = new DataOutputStream(out);
+        Header header = response.getHeader();
+        String message = header.getMessage();
+        sendHeader(dos, message);
+        sendBody(dos, file);
+
+    }
+
+
+}
