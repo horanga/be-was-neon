d950e70bc84784356c7c7e5ce427d0a620dbb01a
Alex <fnelclsrn123@gmail.com>
2024-04-04 11:45:01 +0900

    feat: 지원하는 콘텐츠 타입에 해당하지 않는 예외 추가

--- src/main/java/http/request/message/ContentType.java
+++ src/main/java/http/request/message/ContentType.java
@@ -1,32 +1,38 @@
 package http.request.message;
 
+import exception.InvalidContentTypeException;
+
 import java.util.stream.Stream;
 
 public enum ContentType {
-
     HTML("html", "text/html;charset=utf-8"),
     CSS("css", "text/css;charset=utf-8"),
     JS("js", "application/javascript;charset=utf-8"),
     ICO("ico", "image/x-icon"),
     PNG("png", "image/png"),
     JPG("jpg", "image/jpeg"),
+<<<<<<< HEAD
     SVG("svg", "image/svg+xml"),
     NOFile("","" );
 
+=======
+    SVG("svg", "image/svg+xml");
+>>>>>>> e10f0cf (feat: 지원하는 콘텐츠 타입에 해당하지 않는 예외 추가)
     private final String contentType;
     private final String mimeType;
-
     ContentType(String contentType, String mimeType) {
         this.contentType = contentType;
         this.mimeType = mimeType;
     }
 
-    public static String getMimeType(String requestLine) {
+    public static ContentType getMimeType(String requestLine) {
+
         return Stream.of(ContentType.values())
                 .filter(i -> requestLine.contains(i.contentType))
                 .findFirst()
-                .map(i -> i.mimeType)
-                .orElse(NOFile.mimeType);
+                .orElseThrow(() -> new InvalidContentTypeException("Invalid content type: " + requestLine));
     }
-
+    public String getMimeType() {
+        return mimeType;
+    }
 }
