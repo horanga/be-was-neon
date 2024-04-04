596d6ecc34bfa04e6b39e6d26d209fa13114398c
Alex <fnelclsrn123@gmail.com>
2024-04-04 11:24:14 +0900

    refactor: 쿠키 DB생성

--- src/main/java/http/ClientDatabase.java
+++ src/main/java/http/ClientDatabase.java
@@ -1,8 +1,17 @@
 package http;
 
 import login.Cookie;
+
+import java.util.Map;
+
 public interface ClientDatabase {
     void addCookie(String type, Cookie cookie);
 
     Cookie getCookie(String type);
+
+    void invalidateCookie(String type);
+    public Map<String, Cookie> getCookieDatabase();
+
+    void clear();
+    boolean isLoginCookieExisted();
 }
