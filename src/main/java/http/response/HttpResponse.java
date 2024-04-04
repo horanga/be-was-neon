2fb824f9bc73099bdf95a3767221eb2297cc6784
Alex <fnelclsrn123@gmail.com>
2024-04-04 11:47:37 +0900

    refactor: response 생성 로직 캡슐화

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,22 +1,38 @@
 package http.response;
 
-import http.request.message.RequestMessage;
+import http.request.HttpRequest;
+import http.response.header.Header;
+import http.response.header.HttpVersion;
+import http.response.header.StatusCode;
 
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+
+import static http.response.Path.RELATIVE_PATH;
 
 public interface HttpResponse {
-    public Response respond(File file, RequestMessage requestMessage) throws IOException, URISyntaxException;
 
-    default byte[] getRequestedFile(java.io.File file) throws IOException {
+    Response getResponse(File file, HttpRequest httpRequest) throws IOException;
 
-        byte[] data = new byte[(int) file.length()];
-        try (FileInputStream fis = new FileInputStream(file)) {
-            fis.read(data);
-        }
-        return data;
+    default byte[] readFile(File file) throws IOException {
+        StaticFileReader fileReader = new StaticFileReader();
+        return fileReader.readFile(file);
+    }
+    default Response generateResponse(HttpRequest httpRequest, byte[] files, StatusCode code) throws IOException {
+        String mimeType = httpRequest.getMimeType();
+        int lengthOfBodyContent = files.length;
+        Header header = new Header(HttpVersion.V_11, code, mimeType, lengthOfBodyContent);
+
+        return new Response(header, files);
+    }
+
+    default Response generateRedirectResponse(HttpRequest httpRequest, byte[] files, String location) throws IOException {
+        String mimeType = httpRequest.getMimeType();
+        Header header = Header.redirectHeader(HttpVersion.V_11, mimeType, location);
+
+        return new Response(header, files);
     }
 }
+
