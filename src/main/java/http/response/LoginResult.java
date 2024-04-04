8a3a08eb0b4c3885b768cf93c8bf3af02a750c57
Alex <fnelclsrn123@gmail.com>
2024-04-04 11:47:37 +0900

    refactor: response 생성 로직 캡슐화

--- src/main/java/http/response/LoginResult.java
+++ src/main/java/http/response/LoginResult.java
@@ -1,10 +1,9 @@
 package http.response;
 
-import db.ClientDatabaseImpl;
 import http.ClientDatabase;
+import http.UserDatabase;
+import http.dynamichttp.DynamicHttpGenerator;
 import http.request.HttpRequest;
-import http.response.header.Header;
-import http.response.header.HttpVersion;
 import http.response.header.StatusCode;
 import login.Cookie;
 import login.LoginManager;
@@ -14,55 +13,79 @@ import model.User;
 import java.io.File;
 import java.io.IOException;
 
-import static http.response.resource.File.RELATIVE_PATH;
+import static http.response.Path.*;
 
 public class LoginResult implements HttpResponse {
-
-    public static final String LOGIN_SUCCESS = "main/index.html";
-    final String LOGIN_FAIL = "main/login_failed.html";
     Cookie cookie;
+    ClientDatabase clientDatabase;
+    UserDatabase userDatabase;
 
-    //액션과 계산을 분리
+    public LoginResult(ClientDatabase clientDatabase, UserDatabase userDatabase) {
+        this.clientDatabase = clientDatabase;
+        this.userDatabase = userDatabase;
+    }
+
     @Override
-    public Response generateResponse(File file, HttpRequest httpRequest) throws IOException {
+    public Response getResponse(File file, HttpRequest httpRequest) throws IOException {
 
-        String[] loginInfo = httpRequest.getRequestBody().getUserInfo();
-        return login(loginInfo) ?
+        String[] loginInfo = httpRequest.getUserInfo();
+
+        return login(loginInfo[0], loginInfo[1]) ? //id와 비밀번호
                 success(httpRequest, User.loginInfo(loginInfo[0], loginInfo[1]), this) :
                 fail(httpRequest);
     }
 
-    public boolean login(String[] loginInfo) throws IOException {
-        LoginManager loginManager = new LoginManager();
-        return loginManager.login(loginInfo[0], loginInfo[1]); //id와 비밀번호
+    boolean login(String id, String password){
+        return new LoginManager().login(id, password);
+
     }
 
     Response success(HttpRequest httpRequest, User user, LoginResult response) throws IOException {
         SessionManager sessionManager = new SessionManager();
-        sessionManager.createSession(user, response);//액션
-
-        byte[] file = readFile(new File(RELATIVE_PATH, LOGIN_SUCCESS));//액션
-        int lengthOfBodyContent = file.length;//액션
-        String mimeType = httpRequest.getRequestHeader().getMimeType();//읽기
-        Header header
-                = Header.redirectHeader(HttpVersion.V_11, mimeType, "http://localhost:8080/index.html");
-        new Header(HttpVersion.V_11, StatusCode.OK, mimeType, lengthOfBodyContent);
-        return new Response(header, file);
+        sessionManager.createSession(user, response);
+        User userInfo = getUserInfo(httpRequest, user, sessionManager);
+        return generateSuccessResponse(httpRequest, userInfo);
     }
 
     Response fail(HttpRequest httpRequest) throws IOException {
-        File filePath = new File(RELATIVE_PATH, LOGIN_FAIL);
-        byte[] file = readFile(filePath);
-        int lengthOfBodyContent = file.length;
-        String mimeType = httpRequest.getRequestHeader().getMimeType();
-        Header header = new Header(HttpVersion.V_11, StatusCode.UNAUTHORIZED, mimeType, lengthOfBodyContent);
-
-        return new Response(header, file);
+        byte[] fileToByte = readFile(new File(RELATIVE_PATH, LOGIN_FAIL));
+        return generateResponse(httpRequest, fileToByte, StatusCode.UNAUTHORIZED);
     }
 
-    public void addCookie(String type,Cookie cookie) {
-        ClientDatabase clientDatabase = new ClientDatabaseImpl();
+    public void addCookie(String type, Cookie cookie) {
         this.cookie = cookie;
         clientDatabase.addCookie(type, cookie);
     }
+
+    private User getUserInfo(HttpRequest httpRequest, User user, SessionManager sessionManager) {
+        //유저가 가입하고 첫 로그인할 때 httpRequest에 아직 쿠키가 전달이 안 됨.
+        //쿠키에서 sid를 가져올 수 없기 때문에 유저 DB에서 정보를 가져와서 첫 로그인 후 이용자 이름을 표시해줌
+
+        return sessionManager.isSessionExisted()?
+                getUserInfoFromSid(httpRequest, sessionManager):
+                getUserInfoFromDb(user);
+    }
+
+    private User getUserInfoFromSid(HttpRequest httpRequest, SessionManager sessionManager) {
+        User userInfo;
+        String sid = httpRequest.getSid();
+        userInfo = sessionManager.getUserInfo(sid);
+        return userInfo;
+    }
+
+    private User getUserInfoFromDb(User user) {
+        return userDatabase.findUserById(user.getUserId()).get();
+    }
+    private Response generateSuccessResponse(HttpRequest httpRequest, User userInfo) throws IOException {
+        byte[] modifiedFile = generateDynamicHtmlWithUserInfo(userInfo);
+
+        return generateRedirectResponse(httpRequest, modifiedFile,"http://localhost:8080/index.html");
+    }
+
+    private byte[] generateDynamicHtmlWithUserInfo(User userInfo) throws IOException {
+        DynamicHttpGenerator generator = new DynamicHttpGenerator();
+        byte[] fileToByte = readFile(new File(RELATIVE_PATH, LOGIN_SUCCESS));
+        return generator.generateHtmlForUserId(fileToByte, userInfo);
+    }
+
 }
--- src/main/java/http/response/Membership.java
+++ src/main/java/http/response/Membership.java
@@ -1,8 +1,7 @@
 package http.response;
 
-import http.Database;
+import http.UserDatabase;
 import http.request.HttpRequest;
-import http.request.message.RequestLine;
 import http.response.header.Header;
 import http.response.header.HttpVersion;
 import model.User;
@@ -10,26 +9,24 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class Membership implements HttpResponse {
 
-    Database database;
+    UserDatabase userDatabase;
 
-    public Membership(Database database) {
-        this.database = database;
+    public Membership(UserDatabase userDatabase) {
+        this.userDatabase = userDatabase;
     }
 
     @Override
-    public Response generateResponse(File file, HttpRequest httpRequest) throws IOException{
+    public Response getResponse(File file, HttpRequest httpRequest) throws IOException{
         String[] userInfo = httpRequest.getRequestBody().getUserInfo();
-        database.addUser(new User(userInfo[0], userInfo[1], userInfo[2], userInfo[3]));
+        userDatabase.addUser(new User(userInfo[0], userInfo[1], userInfo[2], userInfo[3]));
         //userId, password, name, email순으로 저장한다.
         LoggerFactory.getLogger("RequestHandler").debug("회원가입이 완료됐습니다.");
-        byte[] files = readFile(file);
-        String mimeType = httpRequest.getRequestHeader().getMimeType();
+        byte[] fileToBytes = readFile(file);
 
-        Header header = Header.redirectHeader(HttpVersion.V_11, mimeType, "http://localhost:8080/index.html");
-        return new Response(header, files);
+        return generateRedirectResponse(httpRequest, fileToBytes,"http://localhost:8080/index.html");
+
     }
 }
