029dcd1f15771e659e1cb7beb547450edf59a4c7
Alex <fnelclsrn123@gmail.com>
2024-04-04 11:51:15 +0900

    refactor: responseTo 메서드 구현 부분 추상화

--- src/main/java/http/response/ResponseManager.java
+++ src/main/java/http/response/ResponseManager.java
@@ -11,24 +11,37 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import static http.response.Path.NOT_FOUND_ERROR;
+import static http.response.Path.RELATIVE_PATH;
+
 public class ResponseManager {
 
     ResponseSender sender;
 
     public void respondTo(HttpRequest httpRequest, OutputStream out) throws IOException {
         RequestLine requestLine = httpRequest.getRequestLine();
-        String[] uri = httpRequest.getRequestLine().getUri();
-        File file = FileType.getFile(requestLine).getFile(uri);
-
-        HttpResponse httpResponse = ResponseType.chooseResponse(httpRequest).getHttpResponse();
-        Response response = httpResponse.generateResponse(file, httpRequest);
-
+        File file = getFile(httpRequest, requestLine);
+        Response response = getResponse(httpRequest, file);
+        chooseSenderType(requestLine);
+        sender.sendResponse(response.getFileAsByte(), response, out);
+    }
+    private void chooseSenderType(RequestLine requestLine) {
         if (requestLine.isPostRequest()) {
             sender = new RedirectionSender();
         } else {
             sender = new NormalResponseSender();
         }
+    }
 
-        sender.sendResponse(response.getFile(), response, out);
+    private Response getResponse(HttpRequest httpRequest, File file) throws IOException {
+        HttpResponse httpResponse = ResponseType.chooseResponse(httpRequest).
+                getHttpResponse();
+        return httpResponse.getResponse(file, httpRequest);
+    }
+
+    private File getFile(HttpRequest httpRequest, RequestLine requestLine) {
+        String[] uri = httpRequest.getUri();
+        return  FileType.getFile(requestLine).
+                getFile(uri);
     }
 }
